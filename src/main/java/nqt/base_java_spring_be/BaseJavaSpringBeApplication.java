b8eabd7dbecd16bd1d3f47447a045681fd2c40e8
tinhtan2505 <61924615+tinhtan2505@users.noreply.github.com>
2025-08-29 23:09:38 +0700

    Update BaseJavaSpringBeApplication.java

--- src/main/java/nqt/base_java_spring_be/BaseJavaSpringBeApplication.java
+++ src/main/java/nqt/base_java_spring_be/BaseJavaSpringBeApplication.java
@@ -2,7 +2,9 @@ package nqt.base_java_spring_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BaseJavaSpringBeApplication {
 
