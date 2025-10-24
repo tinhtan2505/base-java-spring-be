0c3d2faa8087b24c07422e74d177cd6f435378d0
tinhtan2505 <61924615+tinhtan2505@users.noreply.github.com>
2025-10-24 22:23:42 +0700

    clean code

--- src/main/java/nqt/base_java_spring_be/controller/ProjectController.java
+++ src/main/java/nqt/base_java_spring_be/controller/ProjectController.java
@@ -5,21 +5,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import nqt.base_java_spring_be.dto.CustomResponse;
 import nqt.base_java_spring_be.dto.request.ProjectCreateRequest;
-import nqt.base_java_spring_be.dto.request.VnpayCallbackRequest;
-import nqt.base_java_spring_be.dto.response.VnpayCallbackResponse;
 import nqt.base_java_spring_be.entity.Project;
 import nqt.base_java_spring_be.service.iservices.ProjectService;
-import nqt.base_java_spring_be.utils.ErrorUtils;
-import nqt.base_java_spring_be.utils.ThanhToanOnlineUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
--- src/main/java/nqt/base_java_spring_be/service/iservices/IPaymentListenerBidvService.java
+++
@@ -1,7 +0,0 @@
-package nqt.base_java_spring_be.service.iservices;
-
-import java.util.List;
-
-public interface IPaymentListenerBidvService {
-    void getAll();
-}
